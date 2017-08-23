0ce93296e1f753dc0ea9e5594972327f6b1f5882
Eric Carmichael <eric@ckcollab.com>
2017-08-22 22:49:57 -0700

    fix package names

--- src/apps/api/views/competitions.py
+++ src/apps/api/views/competitions.py
@@ -1,14 +1,9 @@
-from rest_framework.generics import GenericAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
-from rest_framework.viewsets import ViewSet, ModelViewSet, GenericViewSet
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from api import serializers
 from competitions.models import Competition
 
 
-# class CompetitionViewSet(ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
-# class CompetitionViewSet(APIView):
 class CompetitionViewSet(ModelViewSet):
     queryset = Competition.objects.all()
     serializer_class = serializers.CompetitionSerializer
