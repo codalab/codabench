1139d58011a627cc8bc53658d340dfcec851f303
Eric Carmichael <eric@ckcollab.com>
2017-08-28 11:21:35 -0700

    fix syntax error

--- src/apps/api/views/search.py
+++ src/apps/api/views/search.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def query(request):
-    client = Elasticsearch([settings.])
+    client = Elasticsearch()
     s = Search(using=client)
 
     # Do filters
