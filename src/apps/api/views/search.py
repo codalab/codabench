2090effb799665ce9c7037c500e72e8943a10dde
Eric Carmichael <eric@ckcollab.com>
2017-08-28 11:05:02 -0700

    remove cruft

--- src/apps/api/views/search.py
+++ src/apps/api/views/search.py
@@ -1,30 +1,8 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
+
 from rest_framework.decorators import api_view
-from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
-
-# class SearchAPIView(APIView):
-#
-#     def get(self, request):
-#         client = Elasticsearch()
-#         s = Search(using=client)
-#
-#         # Do filters
-#         if 'q' in request.GET:
-#             s = s.query("match", title=request.GET['q'])
-#
-#         # Get results
-#         results = s.execute()
-#         data = []
-#         for result in results:
-#             # print(result)
-#             data.append({key: result[key] for key in result})
-#
-#         return Response(data)
-
 
 
 @api_view(['GET'])
@@ -44,18 +22,3 @@ def query(request):
         data.append({key: result[key] for key in result})
 
     return Response(data)
-
-
-# class SearchAPIView(RetrieveAPIView):
-#     def get(self, request, format=None):
-#         client = Elasticsearch()
-#         s = Search(using=client)
-#         results = s.execute()
-#
-#         data = []
-#
-#         for result in results:
-#             print(result['created_by'])
-#             data.append({key: result[key] for key in result})
-#
-#         return Response(data)
