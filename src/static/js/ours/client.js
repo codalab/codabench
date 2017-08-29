c7305930382cabb2c57d802564bc3fbd99e47678
Eric Carmichael <eric@ckcollab.com>
2017-08-28 23:00:35 -0700

    converted styles to stylus, setup nice build/watch via npm-watch

--- src/static/js/ours/client.js
+++ src/static/js/ours/client.js
@@ -13,6 +13,6 @@ CODALAB.api = {
         })
     },
     search: function (query) {
-        return CODALAB.api.request('GET', CODALAB.URLS.API + "query/")
+        return CODALAB.api.request('GET', CODALAB.URLS.API + "query/?q=" + query)
     }
 }
