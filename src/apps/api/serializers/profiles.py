2c214d731342f2af688c5da7f2f3347e43f347a8
Eric Carmichael <eric@ckcollab.com>
2017-08-31 12:59:28 -0700

    adds api endpoint to get user profile

---
+++ src/apps/api/serializers/profiles.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import ModelSerializer
+
+
+User = get_user_model()
+
+
+class MyProfileSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'name',
+            'email',
+            'bio'
+        )
--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -2,24 +2,25 @@ from django.conf.urls import url, include
 from rest_framework.routers import SimpleRouter
 from rest_framework_swagger.views import get_swagger_view
 
-from . import views
+from .views import competitions, data, profiles, search
 
-from .views.search import query
+# from .views.search import query
 
 
 router = SimpleRouter()
-router.register('competitions', views.CompetitionViewSet)
-router.register('phases', views.PhaseViewSet)
-router.register('submissions', views.SubmissionViewSet)
-router.register('data', views.DataViewSet)
-router.register('data_groups', views.DataGroupViewSet)
-# router.register(r'groups', views.GroupViewSet)
-# router.register('query', views.SearchAPIView, base_name='Search')
+router.register('competitions', competitions.CompetitionViewSet)
+router.register('phases', competitions.PhaseViewSet)
+router.register('submissions', competitions.SubmissionViewSet)
+router.register('data', data.DataViewSet)
+router.register('data_groups', data.DataGroupViewSet)
+# router.register(r'groups', GroupViewSet)
+# router.register('query', SearchAPIView, base_name='Search')
 # router.register('query', url('^query/', query), base_name='query')
 
 urlpatterns = [
     url('^', include(router.urls)),
-    url('^query/', query),
+    url('^query/', search.query),
+    url('^my_profile/', profiles.GetMyProfile.as_view()),
 
     url('^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
