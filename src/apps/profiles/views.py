f04f61eb134708622ef39c8a879c1a48576b22af
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-09-07 09:47:45 -0700

    Init profiles/sign-up

--- src/apps/profiles/views.py
+++ src/apps/profiles/views.py
@@ -1,16 +1,16 @@
 # from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def sign_up(request):
-    return HttpResponse("Not implemented yet!")
-
-    # if request.method == 'POST':
-    #     form = UserCreationForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         return HttpResponse("User created successfully!")
-    # else:
-    #     form = UserCreationForm()
-    #     return render(request, 'registration/signup.html', {'form': form})
+    # return HttpResponse("Not implemented yet!")
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("User created successfully!")
+    # form at this point contains errors that we want
+    return render(request, 'registration/signup.html', {'form': form})
