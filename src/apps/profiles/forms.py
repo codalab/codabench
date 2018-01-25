fb0a21cd9ba19db907b6d0b697de0110e71b5d20
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-01-25 14:10:48 -0800

    Initial re-style + functionallity

---
+++ src/apps/profiles/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+from .models import User
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Password'})
--- src/apps/profiles/views.py
+++ src/apps/profiles/views.py
@@ -1,10 +1,14 @@
 # from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 # from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
 
 
 def sign_up(request):
-    return HttpResponse("Not implemented yet!")
+    # return HttpResponse("Not implemented yet!")
 
     # if request.method == 'POST':
     #     form = UserCreationForm(request.POST)
@@ -14,3 +18,16 @@ def sign_up(request):
     # else:
     #     form = UserCreationForm()
     #     return render(request, 'registration/signup.html', {'form': form})
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('pages:home')
+
+    form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
