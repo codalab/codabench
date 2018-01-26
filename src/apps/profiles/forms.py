eb588b79279d8db1e88a725eabc49e09207a32d7
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-01-25 19:28:04 -0800

    Manual HTML render, remove python code for placeholder

--- src/apps/profiles/forms.py
+++ src/apps/profiles/forms.py
@@ -10,10 +10,3 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
-        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
-        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Password'})
