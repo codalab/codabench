176f9c15b6b2eee335677af26b74b22e080ea6d9
Tyler <mr.tyler.thomas@gmail.com>
2018-02-05 13:50:20 -0800

    Run npm-watch on runserver (#42)
    * Run npm-watch on runserver
    
    * Update manage.py
    
    Remove unused import

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
-import os
 import sys
+import os
+import subprocess
 
 if __name__ == "__main__":
     sys.path.append('src')
     settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
+
+    if 'runserver' in sys.argv:
+        subprocess.Popen(["npm-watch"])
 
     from django.core.management import execute_from_command_line
 
