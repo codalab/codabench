47a4f8a5d5675298e8deb59c4a6e508fc932f31c
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-02-06 20:57:28 -0800

    Remove pk in context

--- src/apps/competitions/views.py
+++ src/apps/competitions/views.py
@@ -13,17 +13,5 @@ class CompetitionForm(LoginRequiredMixin, TemplateView):
     template_name = 'competitions/form.html'
 
 
-class CompetitionDetail(LoginRequiredMixin, TemplateView, ContextMixin):
+class CompetitionDetail(LoginRequiredMixin, TemplateView):
     template_name = 'competitions/detail.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context['number'] = random.randrange(1, 100)
-        try:
-            my_pk = kwargs.pop('pk')
-            comp = Competition.objects.get(pk=my_pk)
-            context['comp'] = comp
-            context['comp_pk'] = comp.pk
-        except KeyError:
-            print("Key error oh no")
-        return context
