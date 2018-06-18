efc977e323920dacb31885c0d37818b1b8b51ef1
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-06-17 23:37:41 -0700

    Remove print, change OAuth backend name to Chahub

--- src/utils/oauth_backends.py
+++ src/utils/oauth_backends.py
@@ -6,7 +6,7 @@ BASE_URL = settings.SOCIAL_AUTH_CHAHUB_BASE_URL
 
 class ChahubOAuth2(BaseOAuth2):
     """Github OAuth authentication backend"""
-    name = 'codalab'
+    name = 'chahub'
     API_URL = '{}/api/v1/'.format(BASE_URL)
     AUTHORIZATION_URL = '{}/oauth/authorize/'.format(BASE_URL)
     ACCESS_TOKEN_URL = '{}/oauth/token/'.format(BASE_URL)
@@ -21,7 +21,6 @@ class ChahubOAuth2(BaseOAuth2):
         my_profile_url = "{}my_profile/".format(self.API_URL)
         data = self.get_json(my_profile_url, headers={'Authorization': 'Bearer {}'.format(access_token)})
 
-        print("user details", data)
         return {
             'username': data.get('username'),
             'email': data.get('email'),
