43ad6e0881ad863ec578c2151172059cf3803d14
BrendanMyers88 <myersb88@gmail.com>
2019-04-30 12:24:24 -0700

    Re-merging with up-to-date develop branch

--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -1,8 +1,16 @@
 from django.views.generic import TemplateView
 
+from competitions.models import Competition
+
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["featured_competitions"] = Competition.objects.all()[:5]
+
+        return context
 
 
 class OrganizeView(TemplateView):
