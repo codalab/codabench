660f2ea26143676c1e53e7a8d5ab4db9932ba0da
BrendanMyers88 <myersb88@gmail.com>
2019-05-14 14:25:39 -0700

    Fixes logic for tests, Front page styling for login and account creation

--- src/apps/competitions/tests/test_featured_competitions.py
+++ src/apps/competitions/tests/test_featured_competitions.py
@@ -1,10 +1,7 @@
-import datetime
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.utils.timezone import now
 
-from competitions.models import Competition, CompetitionParticipant, Phase, Submission
+from competitions.models import Competition, CompetitionParticipant
 from competitions.utils import get_featured_competitions, get_popular_competitions
 
 User = get_user_model()
@@ -13,52 +10,79 @@ User = get_user_model()
 class FeaturedCompetitionsTests(TestCase):
 
     def setUp(self):
-        '''
+        """
         1. Create two different users
         2. Create few competitions where published is True
         3. Add participants to competitions
         3. Return all competitions
         4. Return competitions with more participants
-        '''
+        """
 
         self.user1 = User.objects.create(email="user1@test.com", username="user1", password="pass")
         self.user2 = User.objects.create(email="user2@test.com", username="user2", password="pass")
         self.user3 = User.objects.create(email="user3@test.com", username="user3", password="pass")
         self.user4 = User.objects.create(email="user4@test.com", username="user4", password="pass")
+        self.user5 = User.objects.create(email="user5@test.com", username="user5", password="pass")
+        self.user6 = User.objects.create(email="user6@test.com", username="user6", password="pass")
+        self.user7 = User.objects.create(email="user7@test.com", username="user7", password="pass")
 
         self.competition1 = Competition.objects.create(title="competition1", created_by=self.user1, published=True)
         self.competition2 = Competition.objects.create(title="competition2", created_by=self.user1, published=True)
         self.competition3 = Competition.objects.create(title="competition3", created_by=self.user1, published=True)
         self.competition4 = Competition.objects.create(title="competition4", created_by=self.user1, published=True)
         self.competition5 = Competition.objects.create(title="competition5", created_by=self.user1, published=True)
-        self.competition_old = Competition.objects.create(
-            title="competition_old",
-            created_by=self.user1,
-            published=True,
-            created_when=now() - datetime.timedelta(days=50)
-        )
+        self.competition6 = Competition.objects.create(title="competition6", created_by=self.user1, published=True)
+        self.competition7 = Competition.objects.create(title="competition7", created_by=self.user1, published=True)
 
-        self.participant1 = CompetitionParticipant.objects.create(user=self.user1, competition=self.competition1)
-        self.participant2 = CompetitionParticipant.objects.create(user=self.user1, competition=self.competition2)
-        self.participant3 = CompetitionParticipant.objects.create(user=self.user2, competition=self.competition2)
-        self.participant4 = CompetitionParticipant.objects.create(user=self.user3, competition=self.competition1)
-        self.participant5 = CompetitionParticipant.objects.create(user=self.user4, competition=self.competition1)
-        self.participant6 = CompetitionParticipant.objects.create(user=self.user4, competition=self.competition2)
-        self.participant7 = CompetitionParticipant.objects.create(user=self.user4, competition=self.competition4)
-        self.participant8 = CompetitionParticipant.objects.create(user=self.user4, competition=self.competition3)
-        self.participant9 = CompetitionParticipant.objects.create(user=self.user4, competition=self.competition4)
-        self.participant10 = CompetitionParticipant.objects.create(user=self.user2, competition=self.competition3)
-        self.participant12 = CompetitionParticipant.objects.create(user=self.user1, competition=self.competition5)
+        # Competition One Participants
+        CompetitionParticipant.objects.create(user=self.user1, competition=self.competition1)
+        CompetitionParticipant.objects.create(user=self.user2, competition=self.competition1)
+        CompetitionParticipant.objects.create(user=self.user3, competition=self.competition1)
+        CompetitionParticipant.objects.create(user=self.user4, competition=self.competition1)
+        CompetitionParticipant.objects.create(user=self.user5, competition=self.competition1)
+        CompetitionParticipant.objects.create(user=self.user6, competition=self.competition1)
+        CompetitionParticipant.objects.create(user=self.user7, competition=self.competition1)
 
-    # Comp 1 = 3
-    # Comp 2 = 3
-    # Comp 3 = 2
-    # Comp 4 = 2
+        # Competition Two Participants
+        CompetitionParticipant.objects.create(user=self.user1, competition=self.competition2)
+        CompetitionParticipant.objects.create(user=self.user2, competition=self.competition2)
+        CompetitionParticipant.objects.create(user=self.user3, competition=self.competition2)
+        CompetitionParticipant.objects.create(user=self.user4, competition=self.competition2)
+        CompetitionParticipant.objects.create(user=self.user5, competition=self.competition2)
 
-    def test_featured_comps(self):
-        featured_list = get_featured_competitions()
-        assert self.competition2 in featured_list
+        # Competition Three Participants
+        CompetitionParticipant.objects.create(user=self.user4, competition=self.competition3)
+        CompetitionParticipant.objects.create(user=self.user2, competition=self.competition3)
+        CompetitionParticipant.objects.create(user=self.user3, competition=self.competition3)
 
-    def test_popular_comps(self):
+        # Competition Four Participants
+        CompetitionParticipant.objects.create(user=self.user4, competition=self.competition4)
+        CompetitionParticipant.objects.create(user=self.user2, competition=self.competition4)
+
+        # Competition Five Participants
+        CompetitionParticipant.objects.create(user=self.user1, competition=self.competition5)
+        CompetitionParticipant.objects.create(user=self.user2, competition=self.competition5)
+        CompetitionParticipant.objects.create(user=self.user3, competition=self.competition5)
+
+        # Competition Six Participants
+        CompetitionParticipant.objects.create(user=self.user1, competition=self.competition6)
+        CompetitionParticipant.objects.create(user=self.user2, competition=self.competition6)
+        CompetitionParticipant.objects.create(user=self.user3, competition=self.competition6)
+        CompetitionParticipant.objects.create(user=self.user4, competition=self.competition6)
+
+        # Competition Seven Participants
+        CompetitionParticipant.objects.create(user=self.user1, competition=self.competition7)
+        CompetitionParticipant.objects.create(user=self.user2, competition=self.competition7)
+        CompetitionParticipant.objects.create(user=self.user3, competition=self.competition7)
+
+    def test_front_page_competitions(self):
         popular_list = get_popular_competitions()
-        assert self.competition5 not in popular_list
+        featured_list = get_featured_competitions(excluded_competitions=popular_list)
+        assert self.competition1 not in featured_list
+        assert self.competition2 not in featured_list
+        assert self.competition6 not in featured_list
+        assert self.competition1 in popular_list
+        assert self.competition2 in popular_list
+        assert self.competition6 in popular_list
+        assert len(popular_list) == 3
+        assert len(featured_list) == 3
--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -1,16 +1,11 @@
 from django.views.generic import TemplateView
 
 from competitions.models import Competition
+from competitions.utils import get_featured_competitions, get_popular_competitions
 
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["featured_competitions"] = Competition.objects.all()[:5]
-
-        return context
 
 
 class OrganizeView(TemplateView):
