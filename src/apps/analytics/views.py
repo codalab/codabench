b859bb4ac1c4b97230e6e59b05617d76d43c084e
Bailey Trefts <gibson.bailey@live.com>
2019-05-30 17:43:02 -0700

    Only superusers can view analytics.

--- src/apps/analytics/views.py
+++ src/apps/analytics/views.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
+
 
 class AnalyticsView(LoginRequiredMixin, TemplateView):
     template_name = 'analytics/analytics.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args,**kwargs)
 
