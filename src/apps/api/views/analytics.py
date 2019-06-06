a91222e7340fd09c003c47556dc7f513ba81f6ea
Bailey Trefts <gibson.bailey@live.com>
2019-06-06 10:13:42 -0700

    Further improved styling.

--- src/apps/api/views/analytics.py
+++ src/apps/api/views/analytics.py
@@ -55,8 +55,8 @@ def analytics_detail(request):
     end_date = request.query_params.get('end_date')
     time_unit = request.query_params.get('time_unit')
 
-    users = build_request_object(User,'date_joined', time_unit, start_date, end_date)
-    competitions = build_request_object(Competition,'created_when', time_unit, start_date, end_date)
+    users = build_request_object(User, 'date_joined', time_unit, start_date, end_date)
+    competitions = build_request_object(Competition, 'created_when', time_unit, start_date, end_date)
     submissions = build_request_object(Submission, 'created_when', time_unit, start_date, end_date)
 
     return Response({
