54f67ec05d8c2ae56e820b7914d719f41c0a03b4
jimmykodes <james.keith.92@gmail.com>
2019-06-17 11:16:11 -0700

    modified argument logic

--- src/apps/commands/management/commands/generate_data.py
+++ src/apps/commands/management/commands/generate_data.py
@@ -13,18 +13,19 @@ class Command(BaseCommand):
         parser.add_argument(
             '-n',
             '--no-admin',
-            type=bool,
+            action='store_true',
             help='Do not create a superuser w/ username "admin"')
         parser.add_argument(
             '-s',
             '--size',
             type=int,
-            help='Determines the size of data to be created, i.e. 5 users, each with 5 comps with 5 phases with 5 submissions.'
-                 'This defaults to 5 if no argument passed')
+            help='Determines the size of data to be created, i.e. 3 users, each with 3 comps with 3 phases with 3 submissions.'
+                 'This defaults to 3')
 
     def handle(self, *args, **kwargs):
-        size = kwargs['size'] if kwargs['size'] else 5
+        size = kwargs.get('size') or 3
         no_admin = kwargs.get('no_admin')
+        print(f'Creating data of size {size} {"without an admin account." if no_admin else "with an admin account." }')
         users = [UserFactory(username='admin', super_user=True) if i == 0 and not no_admin else UserFactory() for i in range(size)]
         for user in users:
             for _ in range(size):
