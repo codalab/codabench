6094d751dd88e30fc0343658f246af9bd9f5f7ce
Eric Carmichael <eric@ckcollab.com>
2019-12-04 09:15:33 -0800

    make import relative

--- src/settings/develop.py
+++ src/settings/develop.py
@@ -1,4 +1,5 @@
-from settings.base import *  # noqa: F401,F403
+from .base import *  # noqa: F401,F403
+
 
 # =============================================================================
 # Whitenoise
