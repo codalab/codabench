935a4df38a94bee22b65214bf3a935eae05229c1
Eric Carmichael <eric@ckcollab.com>
2020-01-29 11:10:32 -0800

    reduce whitespace in objectToFormData helper

--- src/static/js/ours/utils.js
+++ src/static/js/ours/utils.js
@@ -125,25 +125,19 @@ function objectToFormData(obj, form, namespace) {
 
     for (var property in obj) {
         if (obj.hasOwnProperty(property)) {
-
             if (namespace) {
                 formKey = namespace + '[' + property + ']'
             } else {
                 formKey = property
             }
 
-            // if the property is an object, but not a File,
-            // use recursivity.
+            // if the property is an object, but not a File, use recursivity.
             if (typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
-
                 objectToFormData(obj[property], fd, property)
-
             } else {
-
                 // if it's a string or a File object
                 fd.append(formKey, obj[property])
             }
-
         }
     }
 
