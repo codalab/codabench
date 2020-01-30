b9cb164e921aba271a49479b8d2b8e3e0889e220
Eric Carmichael <eric@ckcollab.com>
2020-01-29 21:39:36 -0800

    add test for non-existing participant created for organizer

--- src/apps/api/tests/test_competitions.py
+++ src/apps/api/tests/test_competitions.py
@@ -30,7 +30,19 @@ class CompetitionParticipantTests(APITestCase):
     # TODO: Do we have competition permissions tests?
     # def test_cant_edit_someone_elses_competition?
 
-    def test_adding_organizer_accepts_them_as_participant(self):
+    def test_adding_organizer_accepts_creates_accepted_participant(self):
+        self.client.login(username='creator', password='creator')
+        url = reverse('competition-detail', kwargs={"pk": self.comp.pk})
+
+        # Get comp data to work with
+        data = self._prepare_competition_data(url)
+
+        data["collaborators"] = [self.other_user.pk]
+        resp = self.client.put(url, data=json.dumps(data), content_type="application/json")
+        assert resp.status_code == 200
+        assert CompetitionParticipant.objects.filter(user=self.other_user, competition=self.comp, status=CompetitionParticipant.APPROVED).count() == 1
+
+    def test_adding_organizer_accepts_them_if_they_were_existing_participant(self):
         CompetitionParticipantFactory(
             user=self.other_user,
             competition=self.comp,
