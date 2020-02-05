77e26c35c2733911fafed218cf71fe00788917a2
Eric Carmichael <eric@ckcollab.com>
2020-02-04 22:53:11 -0800

    wahoo, test passes on circle now :) -- must be because we're using docker-compose there with normal dockers and such

--- src/apps/competitions/tests/test_submissions.py
+++ src/apps/competitions/tests/test_submissions.py
@@ -92,7 +92,6 @@ class SubmissionManagerTests(SubmissionTestCase):
         assert subs[0].phase == subs[1].phase
         assert subs[0].data == subs[1].data
 
-    # TODO: Figure out why this test causes CircleCI to freeze
     def test_cancel_submission_sets_status(self):
         sub = self.make_submission()
         assert sub.cancel(), 'Cancel returned False, meaning the submission could not be cancelled when it should'
