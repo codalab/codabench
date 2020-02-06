82ad595fcf1ef1b09c73e22a840133cd6460dd65
Eric Carmichael <eric@ckcollab.com>
2020-02-05 21:52:22 -0800

    improve DefaultUserCreateMixin comment

--- src/apps/api/mixins.py
+++ src/apps/api/mixins.py
@@ -1,11 +1,12 @@
 class DefaultUserCreateMixin:
-    """This will automatically set `your_model.user` to `request.user`. To override which
+    """This will automatically set `YourModel.user` to `request.user`. To override which
     attribute the user is written to, add a `user_field` to your classes Meta information
 
     Example:
         class MySerializer(DefaultUserCreateMixin, ModelSerializer):
             class Meta:
                 model = YourModel
+                # YourModel.owner = a foreign key to request.user
                 user_field = 'owner'
     """
     def create(self, validated_data):
