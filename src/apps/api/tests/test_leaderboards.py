3771c23bc29adfe83b847a33d7e912447dee0640
Eric Carmichael <eric@ckcollab.com>
2020-02-05 16:33:36 -0800

    fix column seq index problem when running all tests together

--- src/apps/api/tests/test_leaderboards.py
+++ src/apps/api/tests/test_leaderboards.py
@@ -15,18 +15,17 @@ class CompetitionLeaderboardStressTests(APITestCase):
         self.comp = CompetitionFactory(created_by=self.creator)
         self.phase = PhaseFactory(competition=self.comp)
         self.leaderboard = LeaderboardFactory(competition=self.comp)
-        ColumnFactory(leaderboard=self.leaderboard)
+        ColumnFactory(leaderboard=self.leaderboard, index=0)  # need to set index here otherwise it's seq
 
         for _ in range(10):
             SubmissionFactory(phase=self.phase, leaderboard=self.leaderboard)
 
     def test_getting_many_submissions_doesnt_cause_too_many_queries(self):
         self.client.login(username='creator', password='creator')
-
         with self.assertNumQueries(7):
             resp = self.client.get(reverse('leaderboard-detail', args=(self.leaderboard.pk,)))
-
             assert resp.status_code == 200
+
 
 # TODO: Test leaderboard viewset permissions!
 # TODO: Test listing all leaderboards isn't a thing?
