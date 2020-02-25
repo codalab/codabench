5a1c4bf6b23ba2664e653611e02aab4ae0b2c4e8
Eric Carmichael <eric@ckcollab.com>
2020-02-25 14:56:31 -0800

    add comments about what fabric is doing

--- fabfile.py
+++ fabfile.py
@@ -4,15 +4,30 @@ import yaml
 from fabric.api import env, run
 
 
+# Load in server config, here's a quick example:
+#   role_name:
+#     hosts:
+#       - ubuntu@12.34.56.78
+#     broker_url: pyamqp://user:pass@host:port/vhost-gpu
+#     is_gpu: true
+#     docker_image: codalab/competitions-v2-compute-worker:nvidia
+#
+# You select the role to run with like s:
+#   $ fab -R role_name <command>
 host_types = yaml.load(open('server_config.yaml').read())
 env.roledefs = host_types
 
 
 def status():
+    """Gets status of all docker containers on server"""
     run("docker ps")
 
 
 def update():
+    """Updates docker image on server and restarts the worker, based on server_config.yaml settings.
+
+    See README (Worker management section) for example settings."""
+    # Ensure that we're referencing one and only 1 role, so we don't accidentally update CPUs to GPUs
     if not env.effective_roles:
         print("ERROR: You must specify a role when running this task. I.e. fab -R some-gpu task_name`")
         return
@@ -20,6 +35,7 @@ def update():
         print("ERROR: Only specify 1 role (because we need 1 broker_url) when running this task")
         return
 
+    # Read settings from our server_config.yaml config
     role = env.effective_roles[0]
     broker_url = env.roledefs[role]["broker_url"]
     is_gpu = env.roledefs[role]["is_gpu"]
@@ -32,6 +48,7 @@ def update():
         docker_process = "docker"
         nvidia_sock = ""
 
+    # Build our docker command ensuring the nvidia socket is attached if we're in gpu mode
     docker_command = f"""{docker_process} run \
         -v /tmp/codalab-v2:/tmp/codalab-v2 \
         -v /var/run/docker.sock:/var/run/docker.sock \
@@ -41,8 +58,7 @@ def update():
         --restart unless-stopped \
         --log-opt max-size=50m \
         --log-opt max-file=3 \
-        {docker_image}
-    """
+        {docker_image}"""
 
     # Stop and remove containers
     run("docker stop $(docker ps -aq)")
