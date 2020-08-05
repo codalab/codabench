b8b14332ef5b8ca22a4a6e33516d3d693f83c585
Logan Ruf <cruf3@wgu.edu>
2020-08-05 13:19:52 -0700

    Flake8

--- src/apps/api/views/competitions.py
+++ src/apps/api/views/competitions.py
@@ -220,9 +220,9 @@ class CompetitionViewSet(ModelViewSet):
         # Query Needed data and filter to what is needed.
         phase_pks = [phase.id for phase in Phase.objects.filter(competition_id=competition.id)]
         submission_query = Submission.objects.filter(Q(phase_id__in=phase_pks) & Q(has_children=False))
-        if submission_query.first() == None:
+        if submission_query.first() is None:
             raise ValidationError("There are no submissions on the leaderboard")
-        
+
         filtered_submission_query = [sub for sub in submission_query if sub.on_leaderboard]
 
         # Build the data needed for the csv's into a dictionary
