937bfa89963140f139d66af64214fa74f0772721
Eric Carmichael <eric@ckcollab.com>
2020-08-11 10:12:11 -0700

    force SSL redirects on production

--- src/settings/production.py
+++ src/settings/production.py
@@ -7,6 +7,13 @@ CORS_ORIGIN_ALLOW_ALL = False
 DEBUG = False
 
 # =========================================================================
+# SSL
+# =========================================================================
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
+
+# =========================================================================
 # Email
 # =========================================================================
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
