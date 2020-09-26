8a4120c22ff3a6d2d62016be43403d91bdb707aa
Logan-Ruf <cloganruf@gmail.com>
2020-09-25 21:08:13 -0700

    Flake*

--- src/apps/api/serializers/competitions.py
+++ src/apps/api/serializers/competitions.py
@@ -1,4 +1,4 @@
-from drf_writable_nested import WritableNestedModelSerializer, NestedUpdateMixin
+from drf_writable_nested import WritableNestedModelSerializer
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
