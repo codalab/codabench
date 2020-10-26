482e1996364a9d6b459d775a1020a11ad79db256
Logan-Ruf <cloganruf@gmail.com>
2020-10-26 16:53:14 -0700

    Fixed Migration Bug

--- src/apps/competitions/migrations/0016_auto_20200724_2354.py
+++ src/apps/competitions/migrations/0016_auto_20200724_2354.py
@@ -16,4 +16,8 @@ class Migration(migrations.Migration):
             name='data',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='submission', to='datasets.Data'),
         ),
+        migrations.AlterUniqueTogether(
+            name='submission',
+            unique_together=set(),
+        ),
     ]
