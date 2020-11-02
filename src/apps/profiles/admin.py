5b02abdf6effbba0a57a376cd902fe4afadce274
Bailey Lind-Trefts <gibson.bailey@live.com>
2020-11-02 15:10:04 -0800

    Clarify comment.

--- src/apps/profiles/admin.py
+++ src/apps/profiles/admin.py
@@ -4,7 +4,7 @@ from .models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    # The following two lines are needed:
+    # The following two lines are needed for Django-su:
     change_form_template = "admin/auth/user/change_form.html"
     change_list_template = "admin/auth/user/change_list.html"
 
