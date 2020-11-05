1b7677f37fd6108e73f95bda7bce9044ea55582a
Logan-Ruf <cloganruf@gmail.com>
2020-11-05 15:29:19 -0800

    Update src/apps/api/pagination.py

--- src/apps/api/pagination.py
+++ src/apps/api/pagination.py
@@ -7,12 +7,6 @@ class BasicPagination(PageNumberPagination):
     max_page_size = 1000
 
 
-class SmallPagination(PageNumberPagination):
-    page_size = 3
-    page_size_query_param = 'page_size'
-    max_page_size = 1000
-
-
 class LargePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
