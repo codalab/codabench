97ded7538d33734ca38349e7aeafa1d3061ef9b2
Logan-Ruf <cloganruf@gmail.com>
2020-11-09 09:53:07 -0800

    Flake8

--- src/apps/api/pagination.py
+++ src/apps/api/pagination.py
@@ -12,6 +12,7 @@ class LargePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 1000
+    
     def get_paginated_response(self, data):
         return Response({
             'next': self.get_next_link(),
