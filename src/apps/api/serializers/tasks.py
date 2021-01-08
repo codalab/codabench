b1197f24c8fc7eda6df88d2505503fb85a8fcc90
Logan-Ruf <cloganruf@gmail.com>
2021-01-07 17:28:21 -0800

    Nevermind

--- src/apps/api/serializers/tasks.py
+++ src/apps/api/serializers/tasks.py
@@ -38,11 +38,10 @@ class SolutionListSerializer(serializers.ModelSerializer):
 
 
 class TaskSerializer(DefaultUserCreateMixin, WritableNestedModelSerializer):
-    data_query = Data.objects.all()
-    input_data = serializers.SlugRelatedField(queryset=data_query, required=False, allow_null=True, slug_field='key')
-    ingestion_program = serializers.SlugRelatedField(queryset=data_query, required=False, allow_null=True, slug_field='key')
-    reference_data = serializers.SlugRelatedField(queryset=data_query, required=False, allow_null=True, slug_field='key')
-    scoring_program = serializers.SlugRelatedField(queryset=data_query, required=False, allow_null=True, slug_field='key')
+    input_data = serializers.SlugRelatedField(queryset=Data.objects.all(), required=False, allow_null=True, slug_field='key')
+    ingestion_program = serializers.SlugRelatedField(queryset=Data.objects.all(), required=False, allow_null=True, slug_field='key')
+    reference_data = serializers.SlugRelatedField(queryset=Data.objects.all(), required=False, allow_null=True, slug_field='key')
+    scoring_program = serializers.SlugRelatedField(queryset=Data.objects.all(), required=False, allow_null=True, slug_field='key')
     validated = serializers.SerializerMethodField()
     shared_with = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, required=False)
 
