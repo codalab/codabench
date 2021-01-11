d8802ec20136766090837515ac194f83b991e153
Logan-Ruf <cloganruf@gmail.com>
2021-01-11 14:27:27 -0800

    Submission Logs now save to redis

--- src/apps/competitions/consumers.py
+++ src/apps/competitions/consumers.py
@@ -5,6 +5,7 @@ import os
 import aiofiles
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
+from django.core.cache import cache
 
 from competitions.models import Submission
 from utils.data import make_url_sassy
@@ -39,16 +40,13 @@ class SubmissionIOConsumer(AsyncWebsocketConsumer):
         if sub.phase.hide_output and not sub.phase.competition.user_has_admin_permission(user_pk):
             return
 
-        submission_output_path = os.path.join(settings.TEMP_SUBMISSION_STORAGE, f"{submission_id}.txt")
-        os.makedirs(os.path.dirname(submission_output_path), exist_ok=True)
         data = json.loads(text_data)
         if data['kind'] == 'detailed_result_update':
             data['result_url'] = make_url_sassy(Submission.objects.get(id=submission_id).detailed_result.name)
             # update text data to include the newly added sas url for retrieval on page refresh
             text_data = json.dumps(data)
 
-        async with aiofiles.open(submission_output_path, 'a+') as f:
-            await f.write(f'{text_data}\n')
+        cache.set(f'submission-{submission_id}-log', f'{text_data}\n', 3600*25)
 
         await self.channel_layer.group_send(f"submission_listening_{user_pk}", {
             'type': 'submission.message',
@@ -89,10 +87,8 @@ class SubmissionOutputConsumer(AsyncWebsocketConsumer):
             submissions = Submission.objects.filter(id__in=submission_ids, owner=self.scope["user"])
 
             for sub in submissions:
-                text_path = os.path.join(settings.TEMP_SUBMISSION_STORAGE, f"{sub.id}.txt")
-                if os.path.exists(text_path):
-                    with open(text_path) as f:
-                        text = f.read()
+                text = cache.get(f'submission-{sub.id}-log')
+                if text:
                     await self.group_send(text, sub.id, full_text=True)
 
     async def submission_message(self, event):
