07f1ea7622f918592c485c7466c7b3e9652b4f1d
Adrien Pavão <adrien.pavao@gmail.com>
2023-03-15 15:57:21 +0100

    Flake8 formatting

--- src/apps/profiles/forms.py
+++ src/apps/profiles/forms.py
@@ -1,17 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    email = forms.EmailField(
+        max_length=254, help_text="Required. Inform a valid email address."
+    )
+
     def clean_username(self):
-            data = self.cleaned_data['username']
-            if not data.islower():
-                raise forms.ValidationError("Usernames should be in lowercase")
-            if not data.isalnum():
-                raise forms.ValidationError("Usernames should be alpha numeric and not have special characters.")
-            return data
+        data = self.cleaned_data["username"]
+        if not data.islower():
+            raise forms.ValidationError("Usernames should be in lowercase")
+        if not data.isalnum():
+            raise forms.ValidationError(
+                "Usernames should be alpha numeric and not \
+                have special characters."
+            )
+        return data
+
     class Meta:
+
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ("username", "email", "password1", "password2")
