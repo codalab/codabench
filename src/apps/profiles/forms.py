afe8f62dfdc789c866270ea54710fe9bc0ba6d65
bbearce <bbearce@gmail.com>
2023-03-15 14:40:53 +0000

    validate usernames

--- src/apps/profiles/forms.py
+++ src/apps/profiles/forms.py
@@ -5,7 +5,13 @@ from .models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
+    def clean_username(self):
+            data = self.cleaned_data['username']
+            if not data.islower():
+                raise forms.ValidationError("Usernames should be in lowercase")
+            if not data.isalnum():
+                raise forms.ValidationError("Usernames should be alpha numeric and not have special characters.")
+            return data
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
