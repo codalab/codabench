80a4feb37d98666a0336b504b30babc6ea9c7f20
Benjamin <bbearce@gmail.com>
2023-04-10 22:57:00 -0400

    flask error

--- src/apps/profiles/urls_accounts.py
+++ src/apps/profiles/urls_accounts.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
+# from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
