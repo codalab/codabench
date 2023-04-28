cd6e5e8c61b017a482f488d080c3a98813719273
didayolo <adrien.pavao@gmail.com>
2023-04-28 14:12:05 +0200

    Remove the SNCF bundle e2e test

--- src/tests/functional/test_submissions.py
+++ src/tests/functional/test_submissions.py
@@ -75,9 +75,6 @@ class TestSubmissions(SeleniumTestCase):
         prediction_score = Submission.objects.get(pk=submission_id).scores.first().score
         assert Decimal(self.find('leaderboards table tbody tr:nth-of-type(1) td:nth-of-type(3)').text) == prediction_score
 
-    def test_v15_submission_end_to_end(self):
-        self._run_submission_and_add_to_leaderboard('competition_15.zip', 'submission_15.zip', '*** prediction_score', has_solutions=False)
-
     def test_v15_iris_result_submission_end_to_end(self):
         self._run_submission_and_add_to_leaderboard('competition_15_iris.zip', 'submission_15_iris_result.zip', '======= Set 1 (Iris_test)', has_solutions=False)
 
