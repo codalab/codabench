bdfef570daf6e3cbbcb017bbad3a723984eb8978
Ihsan Ullah <ihsan2131@gmail.com>
2023-06-16 21:22:06 +0500

    cleanup stats

---
+++ src/apps/api/tests/test_cleanup.py
@@ -0,0 +1,59 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+import json
+from factories import (
+    UserFactory,
+    CompetitionFactory,
+    PhaseFactory,
+    TaskFactory,
+    SubmissionFactory,
+    DataFactory
+)
+from competitions.models import Submission
+from datasets.models import Data
+
+
+class CleanUpTests(APITestCase):
+    def setUp(self):
+        # Create a user
+        self.user = UserFactory(username='test_user', password='test_user')
+        # Create a competition
+        self.comp = CompetitionFactory(created_by=self.user)
+        # Create used tasks
+        self.used_tasks = [TaskFactory(created_by=self.user), TaskFactory(created_by=self.user)]
+        # Create unused task
+        self.unused_tasks = [TaskFactory(created_by=self.user), TaskFactory(created_by=self.user)]
+        # Create phase with used tasks
+        self.phase = PhaseFactory(competition=self.comp, tasks=[self.used_tasks])
+
+        # Create used-finished submission
+        self.used_failed_submission = [SubmissionFactory(md5="12345", phase=self.phase, status=Submission.FAILED)]
+
+        # Create unused submission
+        self.unused_submissions = [
+            DataFactory(created_by=self.user, type=Data.SUBMISSION),
+            DataFactory(created_by=self.user, type=Data.SUBMISSION)
+        ]
+
+        # Create unused datasets and programs
+        self.unused_datasets_programs = [
+            DataFactory(created_by=self.user, type=Data.INGESTION_PROGRAM),
+            DataFactory(created_by=self.user, type=Data.SCORING_PROGRAM),
+            DataFactory(created_by=self.user, type=Data.INPUT_DATA),
+            DataFactory(created_by=self.user, type=Data.REFERENCE_DATA),
+            DataFactory(created_by=self.user, type=Data.PUBLIC_DATA)
+        ]
+
+        self.client.login(username='test_user', password='test_user')
+
+    def test_cleanup_stats(self):
+
+        url = reverse('user_quota_cleanup')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+
+        assert content["unused_tasks"] == self.unused_tasks.count()
+        assert content["unused_datasets_programs"] == self.unused_datasets_programs.count()
+        assert content["unused_submissions"] == self.unused_submissions.count()
+        assert content["failed_submissions"] == self.used_failed_submission.count()
