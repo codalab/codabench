05eb103574d8005bfe32a86351a7542e27c61ebd
Ihsan Ullah <ihsan2131@gmail.com>
2023-06-16 22:38:19 +0500

    check response

--- src/apps/api/tests/test_cleanup.py
+++ src/apps/api/tests/test_cleanup.py
@@ -15,37 +15,49 @@ from datasets.models import Data
 
 class CleanUpTests(APITestCase):
     def setUp(self):
+
         # Create a user
-        self.user = UserFactory(username='test_user', password='test_user')
+        user = UserFactory(username='test_user', password='test_user')
+
         # Create a competition
-        self.comp = CompetitionFactory(created_by=self.user)
+        comp = CompetitionFactory(created_by=user)
+
         # Create used tasks
-        self.used_tasks = [TaskFactory(created_by=self.user), TaskFactory(created_by=self.user)]
+        self.used_tasks = [
+            TaskFactory(created_by=user),
+            TaskFactory(created_by=user)
+        ]
+
         # Create unused task
-        self.unused_tasks = [TaskFactory(created_by=self.user), TaskFactory(created_by=self.user)]
+        self.unused_tasks = [
+            TaskFactory(created_by=user),
+            TaskFactory(created_by=self.user)
+        ]
+
         # Create phase with used tasks
-        self.phase = PhaseFactory(competition=self.comp, tasks=self.used_tasks)
+        phase = PhaseFactory(competition=comp, tasks=self.used_tasks)
 
         # Create used-failed submission
         self.failed_submissions = [SubmissionFactory(
-            phase=self.phase,
-            owner=self.user,
+            phase=phase,
+            owner=user,
             status=Submission.FAILED,
             secret='7df3600c-1234-5678-bbc8-bbe91f42d875'
         )]
+
         # Create unused submission
         self.unused_submissions = [
-            DataFactory(created_by=self.user, type=Data.SUBMISSION),
-            DataFactory(created_by=self.user, type=Data.SUBMISSION)
+            DataFactory(created_by=user, type=Data.SUBMISSION),
+            DataFactory(created_by=user, type=Data.SUBMISSION)
         ]
 
         # Create unused datasets and programs
         self.unused_datasets_programs = [
-            DataFactory(created_by=self.user, type=Data.INGESTION_PROGRAM),
-            DataFactory(created_by=self.user, type=Data.SCORING_PROGRAM),
-            DataFactory(created_by=self.user, type=Data.INPUT_DATA),
-            DataFactory(created_by=self.user, type=Data.REFERENCE_DATA),
-            DataFactory(created_by=self.user, type=Data.PUBLIC_DATA)
+            DataFactory(created_by=user, type=Data.INGESTION_PROGRAM),
+            DataFactory(created_by=user, type=Data.SCORING_PROGRAM),
+            DataFactory(created_by=user, type=Data.INPUT_DATA),
+            DataFactory(created_by=user, type=Data.REFERENCE_DATA),
+            DataFactory(created_by=user, type=Data.PUBLIC_DATA)
         ]
 
         self.client.login(username='test_user', password='test_user')
@@ -58,6 +70,7 @@ class CleanUpTests(APITestCase):
         assert resp.status_code == 200
 
         content = json.loads(resp.content)
+        print(content)
         assert content["unused_tasks"] == len(self.unused_tasks)
         assert content["unused_datasets_programs"] == len(self.unused_datasets_programs)
         assert content["unused_submissions"] == len(self.unused_submissions)
