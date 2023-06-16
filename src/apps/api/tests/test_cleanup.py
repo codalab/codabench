d88d45832501ab80a29e0c1ddb34d6ad54c62211
Ihsan Ullah <ihsan2131@gmail.com>
2023-06-16 23:37:41 +0500

    delete tests added

--- src/apps/api/tests/test_cleanup.py
+++ src/apps/api/tests/test_cleanup.py
@@ -72,3 +72,63 @@ class CleanUpTests(APITestCase):
         assert content["unused_datasets_programs"] == len(self.unused_datasets_programs)
         assert content["unused_submissions"] == len(self.unused_submissions)
         assert content["failed_submissions"] == len(self.failed_submissions)
+
+    def test_delete_unused_tasks(self):
+
+        url = reverse('delete_unused_tasks')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["success"]
+        assert content["message"] == "Unused tasks deleted successfully"
+
+        url = reverse('user_quota_cleanup')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["unused_tasks"] == 0
+
+    def test_delete_unused_datasets(self):
+
+        url = reverse('delete_unused_datasets')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["success"]
+        assert content["message"] == "Unused datasets and programs deleted successfully"
+
+        url = reverse('user_quota_cleanup')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["unused_datasets_programs"] == 0
+
+    def test_delete_unused_submissions(self):
+
+        url = reverse('delete_unused_submissions')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["success"]
+        assert content["message"] == "Unused submissions deleted successfully"
+
+        url = reverse('user_quota_cleanup')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["unused_submissions"] == 0
+
+    def test_delete_failed_submissions(self):
+
+        url = reverse('delete_failed_submissions')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["success"]
+        assert content["message"] == "Failed submissions deleted successfully"
+
+        url = reverse('user_quota_cleanup')
+        resp = self.client.get(url)
+        assert resp.status_code == 200
+        content = json.loads(resp.content)
+        assert content["failed_submissions"] == 0
