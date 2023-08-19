aa83e4be49fe6e93607f5a07e261e6ae7b9640bb
Ihsan Ullah <ihsan2131@gmail.com>
2023-08-19 14:16:51 +0500

    commented code removed

--- src/static/js/ours/latex_markdown_html.js
+++ src/static/js/ours/latex_markdown_html.js
@@ -1,34 +1,4 @@
-// // Function to render Markdown and HTML content and return updated content
-// function renderMarkdownAndHTML(content) {
-//     const parsedHtml = new DOMParser().parseFromString(marked(content), "text/html");
-//     return parsedHtml.body.childNodes;
-//   }
-
-
-// // Function to render LaTeX formulas using KaTeX and return updated content
-// function renderLatexInContent(content) {
-//     const parsedHtml = new DOMParser().parseFromString(content, "text/html");
-//     const traverseAndRender = (node) => {
-//       if (node.nodeType === Node.ELEMENT_NODE) {
-//         const latexPattern = /\$\$([\s\S]*?)\$\$|\$([^\$\n]*?)\$/g;
-//         const hasLatex = latexPattern.test(node.textContent);
-//         if (hasLatex) {
-//           const tempDiv = document.createElement('div');
-//           tempDiv.innerHTML = node.innerHTML.replace(latexPattern, (_, formula1, formula2) => {
-//             const formula = formula1 || formula2;
-//             return katex.renderToString(formula, { throwOnError: false });
-//           });
-//           node.innerHTML = tempDiv.innerHTML;
-//         }
-//       }
-//       node.childNodes.forEach(traverseAndRender);
-//     };
-//     traverseAndRender(parsedHtml.body);
-//     return parsedHtml.body.innerHTML;
-//   }
-
-
-
+// Function to render Markdown,  HTML and Latex and return updated content
 function renderMarkdownWithLatex(content) {
     const parsedHtml = new DOMParser().parseFromString(marked(content), "text/html");
   
