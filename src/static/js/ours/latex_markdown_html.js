60e7cb819ebf4aeb0dccc68f08220d124664c3ea
Ihsan Ullah <ihsan2131@gmail.com>
2023-08-19 22:38:56 +0500

    null content condition handled

--- src/static/js/ours/latex_markdown_html.js
+++ src/static/js/ours/latex_markdown_html.js
@@ -1,25 +1,28 @@
 // Function to render Markdown,  HTML and Latex and return updated content
 function renderMarkdownWithLatex(content) {
-    const parsedHtml = new DOMParser().parseFromString(marked(content), "text/html");
+    if(content === null){
+      return []
+    }
+    const parsedHtml = new DOMParser().parseFromString(marked(content), "text/html")
   
     const traverseAndRenderLatex = (node) => {
       if (node.nodeType === Node.ELEMENT_NODE) {
-        const latexPattern = /\$\$([\s\S]*?)\$\$|\$([^\$\n]*?)\$/g;
-        const hasLatex = latexPattern.test(node.textContent);
+        const latexPattern = /\$\$([\s\S]*?)\$\$|\$([^\$\n]*?)\$/g
+        const hasLatex = latexPattern.test(node.textContent)
         if (hasLatex) {
-          const tempDiv = document.createElement('div');
+          const tempDiv = document.createElement('div')
           tempDiv.innerHTML = node.innerHTML.replace(latexPattern, (_, formula1, formula2) => {
-            const formula = formula1 || formula2;
-            return katex.renderToString(formula, { throwOnError: false });
+            const formula = formula1 || formula2
+            return katex.renderToString(formula, { throwOnError: false })
           });
-          node.innerHTML = tempDiv.innerHTML;
+          node.innerHTML = tempDiv.innerHTML
         }
       }
   
-      node.childNodes.forEach(traverseAndRenderLatex);
+      node.childNodes.forEach(traverseAndRenderLatex)
     };
   
-    traverseAndRenderLatex(parsedHtml.body);
+    traverseAndRenderLatex(parsedHtml.body)
   
-    return parsedHtml.body.childNodes;
+    return parsedHtml.body.childNodes
 }
