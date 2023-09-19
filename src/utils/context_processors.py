295cc750694de7660bd3a5c85604c9c808c433a4
bbearce <bbearce@gmail.com>
2023-09-18 21:35:10 -0400

    time limit of the default queue in pop up

--- src/utils/context_processors.py
+++ src/utils/context_processors.py
@@ -20,6 +20,7 @@ def common_settings(request):
 
     return {
         'STORAGE_TYPE': settings.STORAGE_TYPE,
+        'MAX_EXECUTION_TIME_LIMIT': settings.MAX_EXECUTION_TIME_LIMIT,
         'USER_JSON_DATA': json.dumps(user_json_data),
         'RABBITMQ_MANAGEMENT_URL': f"https://{settings.DOMAIN_NAME}:{settings.RABBITMQ_MANAGEMENT_PORT}",
         'FLOWER_URL': f"https://{settings.DOMAIN_NAME}:{settings.FLOWER_PUBLIC_PORT}",
