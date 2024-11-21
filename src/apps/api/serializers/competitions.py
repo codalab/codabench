06b00c659feb2dfa3a80b5d22eb0e35959ac5477
didayolo <adrien.pavao@gmail.com>
2024-11-21 12:16:04 +0100

    Remove useless comments

--- src/apps/api/serializers/competitions.py
+++ src/apps/api/serializers/competitions.py
@@ -348,9 +348,7 @@ class CompetitionDetailSerializer(serializers.ModelSerializer):
     collaborators = CollaboratorSerializer(many=True)
     participant_status = serializers.CharField(read_only=True)
     participants_count = serializers.IntegerField(read_only=True)
-    # participant_count = serializers.IntegerField(read_only=True)
     submissions_count = serializers.IntegerField(read_only=True)
-    # submission_count = serializers.IntegerField(read_only=True)
     queue = QueueSerializer(read_only=True)
     whitelist_emails = serializers.SerializerMethodField()
 
@@ -375,9 +373,7 @@ class CompetitionDetailSerializer(serializers.ModelSerializer):
             'registration_auto_approve',
             'description',
             'participants_count',
-            # 'participant_count',
             'submissions_count',
-            # 'submission_count',
             'queue',
             'enable_detailed_results',
             'show_detailed_results_in_submission_panel',
@@ -434,7 +430,6 @@ class CompetitionDetailSerializer(serializers.ModelSerializer):
 class CompetitionSerializerSimple(serializers.ModelSerializer):
     created_by = serializers.CharField(source='created_by.username', read_only=True)
     owner_display_name = serializers.SerializerMethodField()
-    # participant_count = serializers.IntegerField(read_only=True)
     participants_count = serializers.IntegerField(read_only=True)
 
     class Meta:
@@ -446,7 +441,6 @@ class CompetitionSerializerSimple(serializers.ModelSerializer):
             'owner_display_name',
             'created_when',
             'published',
-            # 'participant_count',
             'participants_count',
             'logo',
             'logo_icon',
