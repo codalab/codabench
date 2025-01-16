09f36115fd81f3f46f75f2c4238804db960e5b30
Benjamin Bearce <bbearce@gmail.com>
2025-01-16 10:31:47 -0500

    consumer async and template static loading changes

--- src/apps/competitions/consumers.py
+++ src/apps/competitions/consumers.py
@@ -1,6 +1,11 @@
 import json
 import logging
 
+
+from asgiref.sync import sync_to_async
+# from channels.db import database_sync_to_async
+
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django_redis import get_redis_connection
 from competitions.models import Submission
@@ -16,7 +21,10 @@ class SubmissionIOConsumer(AsyncWebsocketConsumer):
         submission_id = self.scope['url_route']['kwargs']['submission_id']
         secret = self.scope['url_route']['kwargs']['secret']
         try:
-            Submission.objects.get(pk=submission_id, secret=secret)
+            submission = await sync_to_async(Submission.objects.get)(
+                pk=submission_id, secret=secret
+                )
+            # Submission.objects.get(pk=submission_id, secret=secret)
         except Submission.DoesNotExist:
             return await self.close()
 
@@ -29,7 +37,8 @@ class SubmissionIOConsumer(AsyncWebsocketConsumer):
         logger.debug(f"Received websocket input for user = {user_pk}, submission = {submission_id}, text_data = {text_data}")
 
         try:
-            sub = Submission.objects.get(pk=submission_id)
+            # sub = Submission.objects.get(pk=submission_id)
+            sub = await sync_to_async(Submission.objects.get)(pk=submission_id)
         except Submission.DoesNotExist:
             return await self.close()
 
@@ -38,10 +47,12 @@ class SubmissionIOConsumer(AsyncWebsocketConsumer):
 
         data = json.loads(text_data)
         if data['kind'] == 'detailed_result_update':
-            data['result_url'] = make_url_sassy(Submission.objects.get(id=submission_id).detailed_result.name)
+            sub = await sync_to_async(Submission.objects.get)(id=submission_id)
+            data['result_url'] = make_url_sassy(sub.detailed_result.name)
             # update text data to include the newly added sas url for retrieval on page refresh
             text_data = json.dumps(data)
 
+        
         con = get_redis_connection("default")
         con.append(f':1:submission-{submission_id}-log', f'{text_data}\n')
 
@@ -81,7 +92,10 @@ class SubmissionOutputConsumer(AsyncWebsocketConsumer):
 
         if submission_ids:
             # Filter out submissions not by this user
-            submissions = Submission.objects.filter(id__in=submission_ids, owner=self.scope["user"])
+            # submissions = Submission.objects.filter(id__in=submission_ids, owner=self.scope["user"])
+            submissions = await sync_to_async(lambda: list(Submission.objects.filter(
+                id__in=submission_ids, owner=self.scope["user"]
+            )))()
             con = get_redis_connection("default")
 
             for sub in submissions:
