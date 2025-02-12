47b76a1bb22406514fc25b0d4524bfa95ee3e6a3
Ihsan Ullah <ihsan2131@gmail.com>
2025-02-12 17:42:28 +0500

    user assigned quota will now be in GB instead of bytes

---
+++ src/apps/profiles/quota.py
@@ -0,0 +1,19 @@
+import logging
+from .models import User
+
+logger = logging.getLogger()
+
+
+def reset_all_users_quota_to_gb():
+    """
+    Converts user quota from bytes to GB if it's stored in bytes.
+    Skips users whose quota is already in GB.
+    """
+    users = User.objects.all()
+    affted_users_count = 0
+    for user in users:
+        # If quota is in bytes (greater than 1 GB in bytes)
+        if user.quota > 1000 * 1000 * 1000:
+            user.quota = user.quota / 1e9  # Convert to GB
+            user.save()
+            affted_users_count += 1
