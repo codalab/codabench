5d8df435deaf1fdf9c3998a0136fff6eab0a83f5
didayolo <adrien.pavao@gmail.com>
2025-04-25 20:45:20 +0200

    Make more generic tests (v1, v2)

--- src/apps/competitions/tests/unpacker_test_data.py
+++ src/apps/competitions/tests/unpacker_test_data.py
@@ -213,6 +213,8 @@ PHASES = [
         'starting_kit': None,
         'tasks': [0],
         'status': 'Previous',
+        'hide_output': False,
+        'hide_score_output': False,
     },
     {
         'index': 1,
@@ -230,13 +232,9 @@ PHASES = [
         'tasks': [1],
         'status': 'Current',
         'is_final_phase': True,
+        'hide_output': False,
+        'hide_score_output': False,
     }
-]
-
-V2_SPECIFIC_PHASE_DATA = [
-    # Tuples of (key, value) of data specific to v2 unpacker.
-    ('hide_output', False),
-    ('hide_score_output', False)
 ]
 
 
@@ -247,9 +245,6 @@ def get_phases(version):
         # Make a copy of the list so we aren't mutating the original phases object. May not be strictly necessary,
         # but if we ever write a test comparing v1 to v2 or something, this would avoid bugs.
         v2 = [{k: v for k, v in phase.items()} for phase in PHASES]
-        for phase in v2:
-            for key, value in V2_SPECIFIC_PHASE_DATA:
-                phase[key] = value
         return v2
 
 
