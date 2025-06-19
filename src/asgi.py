9b0d3df94de520452d2ca21bd55bc5244056eb2e
bbearce <bbearce@gmail.com>
2025-06-19 21:15:02 +0000

    removing submissions to pass circleci

--- src/asgi.py
+++ src/asgi.py
@@ -1,14 +1,16 @@
 import os
 import django
+
+# For some reason this usually need to be between lines 2 and 3 to work properly
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 import routing  # or wherever your websocket_urlpatterns are
 
-# For some reason this usually need to be between lines 2 and 3 to work properly
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.develop")
 django.setup()
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
