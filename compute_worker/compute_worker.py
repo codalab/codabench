46bd2ccd86ace3b0452ba2b890dedf5a4a412373
Obada Haddad <obada.haddad@lisn.fr>
2025-06-19 14:24:58 +0200

    Some quick cleanup

--- compute_worker/compute_worker.py
+++ compute_worker/compute_worker.py
@@ -644,7 +644,7 @@ class Run:
             # Don't allow subprocesses to raise privileges
             '--security-opt=no-new-privileges',
 
-            # Set the volumes, ro for Read Only, and z to allow multiple containers to access the volume (useful for podman)
+            # Set the volumes: ro for Read Only,  z to allow multiple containers to access the volume (useful for podman)
             '-v', f'{self._get_host_path(program_dir)}:/app/program:z',
             '-v', f'{self._get_host_path(self.output_dir)}:/app/output:z',
             '-v', f'{self.data_dir}:/app/data:ro',
@@ -654,6 +654,7 @@ class Run:
             
             # Set the user namespace mode for the container
             '--userns', 'host',
+            # Drop all capabilities
             '--cap-drop', 'all', 
             # Don't buffer python output, so we don't lose any
             '-e', 'PYTHONUNBUFFERED=1',
@@ -663,7 +664,7 @@ class Run:
         if os.environ.get("USE_GPU") and CONTAINER_ENGINE_EXECUTABLE=='docker':
             engine_cmd.extend(['--gpus', 'all'])
         # For podman specifically
-        if CONTAINER_ENGINE_EXECUTABLE=='podman' and os.environ.get("USE_GPU"):
+        if os.environ.get("USE_GPU") and CONTAINER_ENGINE_EXECUTABLE=='podman':
             engine_cmd.extend(['--device', 'nvidia.com/gpu=all'])
 
         if kind == 'ingestion':
