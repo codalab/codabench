5bd0bdc491d0ee9b4c5b0acfa4731fc2659ed4c6
Siddharth Singh <165460702+Siddharthh39@users.noreply.github.com>
2025-09-23 10:08:15 +0530

    Remove outdated URL patterns from management app
    Removed obsolete URL patterns for submissions and user management.

--- src/apps/management/urls.py
+++ src/apps/management/urls.py
@@ -5,6 +5,4 @@ from . import views
 app_name = "management"
 
 urlpatterns = [
-    path('submissions', views.SubmissionsView.as_view(), name="submissions"),
-    path('user_management', views.UserManagementView.as_view(), name="user_management")
 ]
