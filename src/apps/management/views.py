24897587337d7574c981674b6c745ad799ef691e
Siddharth Singh <165460702+Siddharthh39@users.noreply.github.com>
2025-09-23 10:07:49 +0530

    Delete unused view classes from views.py
    Removed SubmissionsView and UserManagementView classes.

--- src/apps/management/views.py
+++ src/apps/management/views.py
@@ -1,9 +1,1 @@
 from django.views.generic import TemplateView
-
-
-class SubmissionsView(TemplateView):
-    template_name = 'management/submissions.html'
-
-
-class UserManagementView(TemplateView):
-    template_name = 'management/user_management.html'
