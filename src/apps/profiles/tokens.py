4e10120b27e515214e52b5ced1ab885255dac866
Obada Haddad <obada.haddad@lisn.fr>
2026-02-19 13:48:30 +0100

    flake8 fixes

--- src/apps/profiles/tokens.py
+++ src/apps/profiles/tokens.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
+
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
