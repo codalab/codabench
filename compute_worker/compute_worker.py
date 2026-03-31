7c66bdf3d59bbc382d9dc56766777e72b0914c3a
Jens Behley <jens.behley@igg.uni-bonn.de>
2026-04-01 01:23:14 +0200

    creating directory inside the container in the right place.

--- compute_worker/compute_worker.py
+++ compute_worker/compute_worker.py
@@ -925,15 +925,15 @@ class Run:
         # Take our list of paths and smash 'em together
         path = os.path.join(*paths)
 
+        # Create if necessary
+        os.makedirs(path, exist_ok=True)
+
         # pull front of path, which points to the location inside the container
         path = path[len(Settings.BASE_DIR):]
 
         # add host to front, so when we run commands in the container on the host they
         # can be seen properly
         path = os.path.join(Settings.HOST_DIRECTORY, path)
-
-        # Create if necessary
-        os.makedirs(path, exist_ok=True)
 
         return path
 
