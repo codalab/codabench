824f8aebb3b791ef938afc24c08507e1c82ff1ce
Idir Chikhoune <idir.chikhoune@lisn.fr>
2026-05-27 14:13:26 +0200

    clean feature

--- src/apps/competitions/tasks.py
+++ src/apps/competitions/tasks.py
@@ -3,7 +3,6 @@ import os
 import re
 import traceback
 import zipfile
-import json
 from datetime import timedelta, datetime
 from django.conf import settings
 from io import BytesIO
@@ -12,7 +11,6 @@ from tempfile import TemporaryDirectory, NamedTemporaryFile
 import oyaml as yaml
 import requests
 from celery._state import app_or_default
-from django_redis import get_redis_connection
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.base import ContentFile
 from django.db.models import Subquery, OuterRef, Count, Case, When, Value, F
@@ -42,10 +40,6 @@ from utils.email import codalab_send_markdown_email
 import logging
 
 logger = logging.getLogger(__name__)
-
-r = get_redis_connection("default")
-WORKERS_REGISTRY_KEY = "compute_workers_registry"
-WORKER_HEARTBEAT_TTL = 35
 
 COMPETITION_FIELDS = [
     "title",
