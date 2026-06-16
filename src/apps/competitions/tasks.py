ff231817d7092001891bbe951ee7dfdcd82c8703
Idir Chikhoune <idir.chikhoune@lisn.fr>
2026-06-16 14:16:14 +0200

    Feature group routing for submissions (#2393)
    * backend & frontend OK / TODO: site worker and leaderboad
    
    * site worker sending submissions to group queue OK
    
    * leaderboad group feature
    
    * logs removed
    
    * fix json leaderboard
    
    * Clean up leaderboard ordering logic
    
    * competition queue on groups with out queue
    
    * some bugfix
    
    * UI bugfix
    
    * UI bugfix
    
    * leaderboard groups format parentsubID_groupname
    
    * fix conflicts issues
    
    * resolve conflict
    
    * clean site worker
    
    * branch update and linter fix
    
    * linter fix
    
    * linter fix
    
    * linter fix
    
    * bugfix group form
    
    * adding migrations files
    
    * fix logic to fix tests problem
    
    * fix logic to fix tests problem
    
    * E2E test fixed
    
    * E2E test fixed
    
    * E2E test fixed
    
    * E2E test fixed
    
    * Fix queue name in server status
    
    * fix queues visibility
    
    * fix queue visibility for groups
    
    * Flake8
    
    ---------
    
    Co-authored-by: didayolo <adrien.pavao@gmail.com>

--- src/apps/competitions/tasks.py
+++ src/apps/competitions/tasks.py
@@ -8,14 +8,10 @@ from datetime import timedelta, datetime
 from io import BytesIO
 from tempfile import TemporaryDirectory, NamedTemporaryFile
 
-# import json
-# import urllib
-
 import oyaml as yaml
 import requests
 from celery._state import app_or_default
 from django.conf import settings
-# from django_redis import get_redis_connection
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.base import ContentFile
 from django.db.models import Subquery, OuterRef, Count, Case, When, Value, F
@@ -24,10 +20,9 @@ from django.utils.text import slugify
 from django.utils.timezone import now
 from rest_framework.exceptions import ValidationError
 
-from celery_config import app  # , app_for_vhost
+from celery_config import app
 from competitions.models import Submission, CompetitionCreationTaskStatus, SubmissionDetails, Competition, \
     CompetitionDump, Phase
-# from queues.models import Queue
 from competitions.unpackers.utils import CompetitionUnpackingException
 from competitions.unpackers.v1 import V15Unpacker
 from competitions.unpackers.v2 import V2Unpacker
@@ -36,12 +31,9 @@ from tasks.models import Task
 from datasets.models import Data
 from utils.data import make_url_sassy
 from utils.email import codalab_send_markdown_email
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
 
 import logging
 
-# from utils.worker_utils import WORKER_HEARTBEAT_TTL, WORKERS_REGISTRY_KEY, extract_queue_names, is_compute_worker, known_compute_queue_names
 logger = logging.getLogger(__name__)
 
 COMPETITION_FIELDS = [
@@ -127,6 +119,40 @@ COLUMN_FIELDS = [
 MAX_EXECUTION_TIME_LIMIT = int(os.environ.get('MAX_EXECUTION_TIME_LIMIT', 600))  # time limit of the default queue
 
 
+def _get_user_group_queues(user, competition):
+    all_user_groups = list(
+        competition.participant_groups
+        .filter(user__pk=user.pk)
+        .select_related('queue')
+        .distinct()
+    )
+
+    if not all_user_groups:
+        return []
+
+    groups_with_queue = [g for g in all_user_groups if g.queue_id is not None]
+    has_groups_without_queue = any(g.queue_id is None for g in all_user_groups)
+
+    if not groups_with_queue:
+        return []
+
+    seen_ids = set()
+    queues = []
+    for group in groups_with_queue:
+        if group.queue_id not in seen_ids:
+            seen_ids.add(group.queue_id)
+            queues.append(group.queue)
+
+    if has_groups_without_queue:
+        competition_queue = competition.queue
+        if competition_queue is None:
+            queues.append(None)
+        elif competition_queue.id not in seen_ids:
+            queues.append(competition_queue)
+
+    return queues
+
+
 def _send_to_compute_worker(submission, is_scoring):
     run_args = {
         "user_pk": submission.owner.pk,
@@ -208,24 +234,24 @@ def _send_to_compute_worker(submission, is_scoring):
     time_padding = 60 * 20  # 20 minutes
     time_limit = submission.phase.execution_time_limit + time_padding
 
-    if submission.phase.competition.queue:  # if the competition is running on a custom queue, not the default queue
-        submission.queue = submission.phase.competition.queue
-        run_args['execution_time_limit'] = submission.phase.execution_time_limit  # use the competition time limit
-        submission.save(update_fields=["queue"])
+    effective_queue = submission.queue or submission.phase.competition.queue
+
+    if effective_queue:
+        run_args['execution_time_limit'] = submission.phase.execution_time_limit
+        if submission.queue != effective_queue:
+            submission.queue = effective_queue
+            submission.save(update_fields=["queue"])
+
     if submission.status == Submission.SUBMITTING:
-        # Don't want to mark an already-prepared submission as "submitted" again, so
-        # only do this if we were previously "SUBMITTING"
         submission.status = Submission.SUBMITTED
         submission.save(update_fields=["status"])
 
     def _enqueue_after_commit():
-        # priority of scoring tasks is higher, we don't want to wait around for
-        # many submissions to be scored while we're waiting for results
         priority = 10 if is_scoring else 0
-        if submission.phase.competition.queue:
+        if effective_queue:
             celery_app = app_or_default()
             with celery_app.connection() as new_connection:
-                new_connection.virtual_host = str(submission.phase.competition.queue.vhost)
+                new_connection.virtual_host = str(effective_queue.vhost)
                 task = celery_app.send_task(
                     'compute_worker_run',
                     args=(run_args,),
@@ -289,12 +315,7 @@ def send_child_id(submission, child_id):
 
 @app.task(queue='site-worker', soft_time_limit=60)
 def _run_submission(submission_pk, task_pks=None, is_scoring=False):
-    """This function is wrapped so that when we run tests we can run this function not
-    via celery"""
-    select_models = (
-        'phase',
-        'phase__competition',
-    )
+    select_models = ('phase', 'phase__competition')
     prefetch_models = (
         'details',
         'phase__tasks__input_data',
@@ -306,24 +327,35 @@ def _run_submission(submission_pk, task_pks=None, is_scoring=False):
     submission = qs.get(pk=submission_pk)
 
     if submission.is_specific_task_re_run:
-        # Should only be one task for a specified task submission
         tasks = Task.objects.filter(pk__in=task_pks)
     elif task_pks is None:
         tasks = submission.phase.tasks.all()
     else:
         tasks = submission.phase.tasks.filter(pk__in=task_pks)
+    tasks = list(tasks.order_by('pk'))
 
-    tasks = tasks.order_by('pk')
+    if submission.parent is None and not is_scoring:
+        group_queues = _get_user_group_queues(submission.owner, submission.phase.competition)
+    else:
+        group_queues = []
 
-    if len(tasks) > 1:
-        # The initial submission object becomes the parent submission and we create children for each task
+    is_multi_task = len(tasks) > 1
+    is_multi_queue = len(group_queues) > 1
+
+    if is_multi_task or is_multi_queue:
+        if is_multi_task and is_multi_queue:
+            combos = [(task, queue) for task in tasks for queue in group_queues]
+        elif is_multi_task:
+            override = group_queues[0] if group_queues else None
+            combos = [(task, override) for task in tasks]
+        else:
+            combos = [(tasks[0], queue) for queue in group_queues]
+
         submission.has_children = True
         submission.save()
-
         send_parent_status(submission)
 
-        for task in tasks:
-            # TODO: make a duplicate submission method and use it here
+        for task, queue in combos:
             child_sub = Submission(
                 owner=submission.owner,
                 phase=submission.phase,
@@ -331,16 +363,22 @@ def _run_submission(submission_pk, task_pks=None, is_scoring=False):
                 participant=submission.participant,
                 parent=submission,
                 task=task,
-                fact_sheet_answers=submission.fact_sheet_answers
+                fact_sheet_answers=submission.fact_sheet_answers,
+                queue=queue,
             )
             child_sub.save(ignore_submission_limit=True)
             _send_to_compute_worker(child_sub, is_scoring=False)
             send_child_id(submission, child_sub.id)
+
     else:
-        # The initial submission object is the only submission
         if not submission.task:
             submission.task = tasks[0]
             submission.save()
+
+        if group_queues and submission.queue != group_queues[0]:
+            submission.queue = group_queues[0]
+            submission.save(update_fields=['queue'])
+
         _send_to_compute_worker(submission, is_scoring)
 
 
@@ -804,18 +842,3 @@ def submission_status_cleanup():
                 sub.parent.cancel(status=Submission.FAILED)
             else:
                 sub.cancel(status=Submission.FAILED)
-
-
-# -------------------------------------------------
-def _broadcast_worker_state(payload):
-    channel_layer = get_channel_layer()
-    if not channel_layer:
-        return
-
-    async_to_sync(channel_layer.group_send)(
-        "compute_workers",
-        {
-            "type": "worker.health",
-            "worker": payload,
-        },
-    )
