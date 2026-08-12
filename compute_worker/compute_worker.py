67a3b74bb429b04ff683444bef52b5408076fcbb
Idir Chikhoune <idir.chikhoune@lisn.fr>
2026-08-12 11:31:37 +0200

    reintroduced lost comment

--- compute_worker/compute_worker.py
+++ compute_worker/compute_worker.py
@@ -315,6 +315,7 @@ def rewrite_bundle_url_if_needed(url):
 # -----------------------------------------------------------------------------
 @shared_task(name="compute_worker_run")
 def run_wrapper(run_args):
+    # We need to convert the UUID given by celery into a byte like object otherwise things will break
     run_args.update(secret=str(run_args["secret"]))
 
     logger.info(
