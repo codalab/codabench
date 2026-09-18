0fd8fd66cf083608595163e8fae5df42283e13b2
Adrien Pavão <adrien.pavao@gmail.com>
2026-09-18 10:33:59 +0200

    Revert "Compute worker workaround for API throttling"

--- src/apps/api/views/submissions.py
+++ src/apps/api/views/submissions.py
@@ -49,7 +49,7 @@ class SubmissionViewSet(ModelViewSet):
                     hostname = request.data['status_details'].replace('ingestion_hostname-', '')
                     obj.ingestion_worker_hostname = hostname
                     obj.save()
-                # Check scoring hostname
+                # Check socring hostname
                 if request.data['status_details'].find('scoring_hostname') != -1:
                     hostname = request.data['status_details'].replace('scoring_hostname-', '')
                     obj.scoring_worker_hostname = hostname
--- src/settings/base.py
+++ src/settings/base.py
@@ -320,9 +320,9 @@ REST_FRAMEWORK = {
         'api.throttling.UserBurstRateThrottle',
     ),
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100000/min',
+        'anon': '100/day',
         'user': '1000/day',
-        'anon_burst': '60000/min',
+        'anon_burst': '60/min',
         'user_burst': '300/min',
         'competitions_public': '300/day',
     },
